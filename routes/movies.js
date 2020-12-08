53faee830e34dc38accf39dded0f2708d8200155
TATIANA Toro <tatianatorog@gmail.com>
2020-12-07 20:45:06 -0500

    Adding json

--- routes/movies.js
+++ routes/movies.js
@@ -1,15 +1,28 @@
-const { Router } = require('express')
+const { Router } = require("express");
 const router = Router();
 
-const movies = require('../sample.json')
+const movies = require("../sample.json");
 
-
-router.get('/', (req, res) => {
-    res.json(movies)
+router.get("/", (req, res) => {
+  res.json(movies);
 });
-router.post('/', (req, res )=> {
-    console.log(req.body);
-    res.send("recieved")
+router.post("/", (req, res) => {
+  const { title, director, year, rating } = req.body;
+  if (title && director && year && rating) {
+    const id = movies.length + 1;
+    const newMovie = { ...req.body, id };
+    // console.log(newMovie);
+    // res.json("saved");
+    movies.push(newMovies)
+    res.json(movies);
+  } else {
+    // res.send("wrong Request ");
+    res.status(500).json({ error: 'There was an error'});
+    
+  }
 });
+router.delete('/:id', (req, res) => {
+    const {} = req.params;
+})
 
 module.exports = router;
